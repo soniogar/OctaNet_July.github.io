2843591e3dcb40ec0fe546473fdbc0edcaf5c692
Soniya Ogar <114099401+soniogar@users.noreply.github.com>
2023-07-08 15:41:08 +0530

    MY BITDOCX Website

---
+++ forms/about.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php"><center>HOME</center></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        
+      </li>
+        <li class="nav-item">
+        
+      </li>
+
+   
+    </ul>
+    
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1 class="text-uppercase" ></>BIT CANTEEN :) </h1> 
+  <p class="text-capitalize"> We serve good food here. <span class="text-uppercase">order,pay and enjoy the meal</span>.</p> 
+</div>
+<section class="m-5">
+  <div >
+    <h2 class="text-center"> About Us</h2>
+  </div>
+  <div class="row mt-5">
+    <div class="col-lg-6 col-12">
+      <img src="images/lounge3.jpg" class="img-fluid aboutimg" >
+    </div>
+    <div class="col-lg-6 col-12">
+      <h2 class="display-4"> BIT CANTEEN. </h2>
+      <p class="py-4"> Bhilai Institute of Technology is committed to provide value based quality technical education and facilitis to the students to boost there performance. Always continue the climb. It is possible for you to do whatever you choose, if you first get to know who you are and are willing to work with a power that is greater than ourselves to do it. Nothing can be done without hope and confidence. If you can dream it, you can do it. Do it now, not tomorrow. Always continue the climb </p>
+      <a href=https://bitdurg.ac.in/ class="btn btn-success">Wanna Know Me</a>   
+    </div>
+  </div>
+</section>
+
+
+<footer>
+  <div class=" bg-dark text-center text-capitalize">
+    <h5 class="py-3 text-white">@Bitcanteen</h5>
+  </div>
+</footer>
+
+</body>
+</html>
---
+++ resources.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">BIT CANTEEN</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="pay.php"></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php"></a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="https://ads.google.com/intl/en_in/getstarted/?subid=in-en-ha-awa-bk-c-c00!o3~Cj0KCQiA99ybBhD9ARIsALvZavXytwjxwxm0XoXgpvrWFS-B6efAcgDPSE_7lveOdUm0H6Xjc9uTSGYaAizvEALw_wcB~140706620052~aud-618819227993:kwd-94527731~16862088904~592430220682&gclid=Cj0KCQiA99ybBhD9ARIsALvZavXytwjxwxm0XoXgpvrWFS-B6efAcgDPSE_7lveOdUm0H6Xjc9uTSGYaAizvEALw_wcB&gclsrc=aw.ds">Google Ads</a>
+      </li>
+
+   
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1 class="text-uppercase" >BIT CANTEEN :) </h1> 
+  <p class="text-capitalize"> <span class="text-uppercase"></span>.</p> 
+</div>
+<section class="m-5">
+  <div >
+    <h2 class="text-center"> About Us</h2>
+  </div>
+  <div class="row mt-5">
+    <div class="col-lg-6 col-12">
+      <img src="images/ff3.jpg" class="img-fluid aboutimg" >
+    </div>
+    <div class="col-lg-6 col-12">
+      <h2 class="display-4"> BIT CANTEEN. </h2>
+      <p class="py-4"> Bhilai Institute of Technology is committed to provide value based quality technical education and facilitis to the students to boost there performance. Always continue the climb. It is possible for you to do whatever you choose, if you first get to know who you are and are willing to work with a power that is greater than ourselves to do it. Nothing can be done without hope and confidence. If you can dream it, you can do it. Do it now, not tomorrow. Always continue the climb </p>
+      <a href="#" class="btn btn-success">Wanna Know Me</a>   
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+<section class="m-5">
+  <div >
+    <h2 class="text-center"> About Us</h2>
+  </div>
+  <div class="row mt-5">
+    <div class="col-lg-6 col-12">
+      <img src="images/ff3.jpg" class="img-fluid aboutimg" >
+    </div>
+    <div class="col-lg-6 col-12">
+      <h2 class="display-4"> BIT CANTEEN. </h2>
+      <p class="py-4"> Bhilai Institute of Technology is committed to provide value based quality technical education and facilitis to the students to boost there performance. Always continue the climb. It is possible for you to do whatever you choose, if you first get to know who you are and are willing to work with a power that is greater than ourselves to do it. Nothing can be done without hope and confidence. If you can dream it, you can do it. Do it now, not tomorrow. Always continue the climb </p>
+      <a href="#" class="btn btn-success">Wanna Know Me</a>   
+    </div>
+  </div>
+</section>
+
+
+
+
+
+<footer>
+  <div class=" bg-dark text-center text-capitalize">
+    <h5 class="py-3 text-white">@Bitcanteen</h5>
+  </div>
+</footer>
+
+</body>
+</html>
